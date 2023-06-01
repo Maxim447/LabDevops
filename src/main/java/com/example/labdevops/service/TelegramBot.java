f09f8ce4c32f55aaa80179b93f10fd672fc82d54
Maxim447 <maximbas777@gmail.com>
2023-06-01 13:56:55 +0300

    initial commit

---
+++ src/main/java/com/example/labdevops/service/TelegramBot.java
@@ -0,0 +1,48 @@
+package com.example.labdevops.service;
+
+import com.example.labdevops.config.Config;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final Config config;
+
+    public TelegramBot(Config config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            sendMessage(chat_id, message_text);
+        }
+    }
+
+    private void sendMessage(long chatId, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/test/java/com/example/labdevops/LabDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
